d9528a81cdd7c8a37e4f83b8ac80bf6517e89a91
Vo Duc Huy <voduchuy2001@gmail.com>
2025-09-11 14:29:05 +0700

    first commit

---
+++ src/Application/Todos/Update/UpdateTodoCommand.cs
@@ -0,0 +1,5 @@
+using Application.Abstractions.Messaging;
+
+namespace Application.Todos.Update;
+
+public sealed record UpdateTodoCommand(Guid TodoItemId, string Description) : ICommand;
---
+++ src/SharedKernel/IDomainEventHandler.cs
@@ -0,0 +1,7 @@
+namespace SharedKernel;
+
+public interface IDomainEventHandler<in T>
+    where T : IDomainEvent
+{
+    Task Handle(T domainEvent, CancellationToken cancellationToken);
+}
